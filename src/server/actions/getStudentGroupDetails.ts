766d9c122612db5a6f3114fe51a6085eb52c1e49
sahil-1012 <sahil10.jai@gmail.com>
2025-05-04 23:19:57 +0530

    Fixed

--- src/server/actions/getStudentGroupDetails.ts
+++ src/server/actions/getStudentGroupDetails.ts
@@ -1,12 +1,17 @@
 import { getDB } from "@/lib/db";
+import { getCookie } from '../../utils/cookie';
 
 export const getStudentGroupDetails = async (studentId: string) => {
     try {
+    const token = jwt.sign({ userId: user._id.toString(), [isEmployee ? 'employeeId' : 'studentId']: userId }, JWT_SECRET, { expiresIn: '7d' });
+
+        const studentIdToken = await getCookie('authToken');
+        get user_id from the toe n
         const db = await getDB();
         const studentsCol = db.collection('students');
         const groupsCol = db.collection('groups');
 
-        const student = await studentsCol.findOne({ studentId });
+        const student = await studentsCol.findbyId(user_id);
         if (!student) return { success: false, message: '❌ Student not found.' };
 
         const group = await groupsCol.findOne({ memberIds: student._id });
@@ -18,7 +23,7 @@ export const getStudentGroupDetails = async (studentId: string) => {
                 groupStatus: group?.groupStatus,
                 projects: group?.projects,
                 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-                members: members.map(({ password, ...rest }) => rest) 
+                members: members.map(({ password, ...rest }) => rest)
             }
         };
 
